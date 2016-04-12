941be1f02eeb19874057e3a22fe9109266baae78
Benjamin Johnson <bkj986@uowmail.edu.au>
2016-04-12 11:25:45 +1000

    Initial Commit

---
+++ GLUTDrawingApp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GLUTDrawingApp
+//
+//  Created by Benjamin Johnson on 12/04/2016.
+//  Copyright © 2016 Benjamin Johnson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
